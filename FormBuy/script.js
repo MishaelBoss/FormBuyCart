b3d6a7d2e2b8fb7939dc42f126e87618fb1487dd
Michael <michaelvoov@yandex.ru>
2023-12-06 22:44:54 +0500

    update3

--- FormBuy/script.js
+++ FormBuy/script.js
@@ -14,34 +14,43 @@ function changeCard(){
     document.getElementById('image-card').innerHTML = str;
     if (str.substring(0, 1) == '4'){
         document.getElementById('op-card').innerHTML = 'VISA';
-        imageElement.src = "imges/visa.png";
+        imageElement.src = "images/visa.png";
     }
     else if (str.substring(0, 1) == '2'){
         document.getElementById('op-card').innerHTML = 'MIR';
+        imageElement.src = "images/mir.png";
     }
     else if (str.substring(0, 2) == '60'){
         document.getElementById('op-card').innerHTML = 'DISCOVER';
+        imageElement.src = "images/discover.png";
     }
     else if (str.substring(0, 2) == '62'){
         document.getElementById('op-card').innerHTML = 'China UnionPay';
+        imageElement.src = "images/UnionPay.png";
     }
     else if (str.substring(0, 1) == '7'){
         document.getElementById('op-card').innerHTML = 'UEK';
+        imageElement.src = "images/xz-bank.jpg";
     }
     else if (maestro.includes(Number(str.substring(0, 2)))){
         document.getElementById('op-card').innerHTML = 'MAESTRO';
+        imageElement.src = "images/maestro.png";
     }
     else if (mastercard.includes(Number(str.substring(0, 2)))){
         document.getElementById('op-card').innerHTML = 'MASTERCARD';
+        imageElement.src = "images/masterCard.png";
     }
     else if (diners.includes(Number(str.substring(0, 2)))){
         document.getElementById('op-card').innerHTML = 'DINERS';
+        imageElement.src = "images/xz-bank.jpg";
     }
     else if (jcb.includes(Number(str.substring(0, 2)))){
         document.getElementById('op-card').innerHTML = 'JCB International';
+        imageElement.src = "images/xz-bank.jpg";
     }
     else if (ae.includes(Number(str.substring(0, 2)))){
         document.getElementById('op-card').innerHTML = 'American Express';
+        imageElement.src = "images/xz-bank.jpg";
     }
     else{
         document.getElementById('op-card').innerHTML = 'NONE';
